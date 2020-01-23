74a57e44a3731321173443b2ad6ce92f33f66f7d
Shreyansh <shreyanshrana@gmail.com>
2020-01-23 11:46:45 +0530

    Navbar

--- src/components/WelcomeWidget/WelcomeWidget.js
+++ src/components/WelcomeWidget/WelcomeWidget.js
@@ -4,7 +4,7 @@ import "./WelcomeWidget.scss";
 let WelcomeWidget = props => {
   let { name } = props;
   return (
-    <div className="test WelcomeWidget">
+    <div className="WelcomeWidget">
       <h1 className="WelcomeWidget__Title">Welcome {name}</h1>
       <p className="WelcomeWidget__Info">
         Today, we have a sale on the premium for Auto Insurance by Bajaj Allianz
