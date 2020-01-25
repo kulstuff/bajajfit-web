c3b55b5d3c93334fd1723eea112fb6cf32e18ec3
shrutisharmaa <shrutisharma.tu@gmail.com>
2020-01-25 11:25:56 +0530

    Leaderboard

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Journal from "./pages/Journal/Journal";
 import "./App.scss";
 import Bajaar from "./pages/Bajaar/Bajaar";
 import Tasks from "./pages/Tasks/Tasks";
+import LeaderBoardWidget from "././components/LeaderboardWidget/LeaderBoardWidget";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-          <Route exact path="/" component={withRouter(Dashboard)} />
+          <Route exact path="/" component={withRouter(LeaderBoardWidget)} />
           <Route exact path="/journal" component={withRouter(Journal)} />
           <Route exact path="/bajaar" component={withRouter(Bajaar)} />
           <Route exact path="/tasks" component={withRouter(Tasks)} />
---
+++ src/components/LeaderboardWidget/LeaderBoardWidget.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./LeaderBoardWidget.scss";
+
+let LeaderBoardWidget = props => {
+  let {
+    Name1,
+    Name2,
+    Name3,
+    Name4,
+    Name5,
+    Name6,
+    HP1,
+    HP2,
+    HP3,
+    HP4,
+    HP5,
+    HP6
+  } = props;
+  return (
+    <div className="LeaderBoardWidget test">
+      <div className="LeaderBoardWidget__LeaderBoard col col-6">
+        Leaderboard
+      </div>
+      <div className="LeaderBoardWidget__Name1 col col-6">{Name1}</div>
+      <div className="LeaderBoardWidget__Name1HP col col-6"> {HP1}</div>
+      <div className="LeaderBoardWidget__Name2 col col-6">{Name2}</div>
+      <div className="LeaderBoardWidget__Name2HP col col-6">{HP2}</div>
+      <div className="LeaderBoardWidget__Name3 col col-6">{Name3}</div>
+      <div className="LeaderBoardWidget__Name3HP col col-6">{HP3}</div>
+      <div className="LeaderBoardWidget__Name4 col col-6">{Name4}</div>
+      <div className="LeaderBoardWidget__Name4HP col col-6">{HP4}</div>
+      <div className="LeaderBoardWidget__Name5 col col-6">{Name5}</div>
+      <div className="LeaderBoardWidget__Name5HP col col-6">{HP5}</div>
+      <div className="LeaderBoardWidget__Name6 col col-6">{Name6}</div>
+      <div className="LeaderBoardWidget__Name6HP col col-6">{HP6}</div>
+    </div>
+  );
+};
+
+export default LeaderBoardWidget;
