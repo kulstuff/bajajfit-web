33c13b2e2bc471cd2d5df84691a443105224c352
Shreyansh <shreyanshrana@gmail.com>
2020-10-20 13:05:24 +0530

    Updates

---
+++ precache-manifest.25ff218addee07c1df548c55161a0ad4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ce84f294d571e4e356aa45c06513bd",
+    "url": "/bajajfit-web/index.html"
+  },
+  {
+    "revision": "92a40d2a9bcea9696e9f",
+    "url": "/bajajfit-web/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "9a4c7c3e99b66112ca07",
+    "url": "/bajajfit-web/static/css/main.710f7d8f.chunk.css"
+  },
+  {
+    "revision": "92a40d2a9bcea9696e9f",
+    "url": "/bajajfit-web/static/js/2.5c915354.chunk.js"
+  },
+  {
+    "revision": "44bb13c376a360a26522c8368cf779a1",
+    "url": "/bajajfit-web/static/js/2.5c915354.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a4c7c3e99b66112ca07",
+    "url": "/bajajfit-web/static/js/main.d938def5.chunk.js"
+  },
+  {
+    "revision": "a3631f51df13fc8d5be7",
+    "url": "/bajajfit-web/static/js/runtime-main.395651a0.js"
+  },
+  {
+    "revision": "02afd4a1c40644c4b91cb14940e56cc4",
+    "url": "/bajajfit-web/static/media/app.02afd4a1.png"
+  },
+  {
+    "revision": "a913146580787dcd6065c2608af10940",
+    "url": "/bajajfit-web/static/media/blob.a9131465.png"
+  },
+  {
+    "revision": "ef015c9d8cb7a878bd54bdf846cd2571",
+    "url": "/bajajfit-web/static/media/life.ef015c9d.png"
+  },
+  {
+    "revision": "ca501de1465a9b4d249fbc4e1829501b",
+    "url": "/bajajfit-web/static/media/map.ca501de1.png"
+  },
+  {
+    "revision": "ec68beb6fef29a8d8e3d54f132dcf52a",
+    "url": "/bajajfit-web/static/media/reading.ec68beb6.png"
+  }
+]);
